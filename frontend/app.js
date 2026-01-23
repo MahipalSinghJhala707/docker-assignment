97d3adb6ff2d1301a898eadeffabb9e4c975325a
MahipalSinghJhala707 <mahipalsinghjhala707@gmail.com>
2026-01-23 12:01:10 +0530

    fix:uninstall node-fetch as node has inbuilt fetch

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const fetch = require("node-fetch");
 
 const app = express();
 
@@ -7,7 +6,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.post("/submit", async (req, res) => {
-  const response = await fetch("http://backend:5000/submit", {
+  const response = await fetch("http://Backend:5000/submit", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(req.body)
