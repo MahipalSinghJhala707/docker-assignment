6d70d1f97182093179b20d814c8b8a7dae87a28f
MahipalSinghJhala707 <mahipalsinghjhala707@gmail.com>
2026-01-23 12:02:51 +0530

    feat: add functionality to save form data in mongodb

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,19 @@
-from flask import Flask 
+from flask import Flask , request , jsonify
 
+
+from dotenv import load_dotenv
+import os
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+load_dotenv()
+
+uri = os.getenv("MONGO_URI")
+
+client = MongoClient(uri, server_api=ServerApi("1"))
+db = client.users
+collection = db["email_pass"]
 
 app = Flask(__name__)
 
@@ -7,6 +21,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "yo this is running!!!"
+
+@app.route("/submit" , methods=["POST"])
+def submit():
+
+    data = request.json
+    collection.insert_one(data)
+    return jsonify({
+        "message": "Data Submitted Successfully"
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
